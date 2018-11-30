1dcd38c683b438b903eff23cb8bed75d7d8245e4
ugur <ugur.kazdal@sefamerve.com>
2018-11-30 14:42:22 +0300

    eski değişkenler silindi

--- index.php
+++ index.php
@@ -3,16 +3,6 @@
 $root = __DIR__ . DIRECTORY_SEPARATOR;
 require $root . "vendor/autoload.php";
 
-// if (count($argv) < 2) {
-//     echo "php imager /tam/klasör/yolu/ şeklinde çalıştırın";
-//     return;
-//
-;
-$file = ''; //file that you wanna compress
-$new_name_image = ''; //name of new file compressed
-$quality = 80; // Value that I chose
-$pngQuality = 9; // Exclusive for PNG files
-$destination = ''; //This destination must be exist on your project
 $images = glob($root . 'images/*.{jpeg,jpg,png,JPG,PNG,JPEG}', GLOB_BRACE);
 
 if (!file_exists('compressed')) {
